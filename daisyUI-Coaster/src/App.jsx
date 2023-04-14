c1da5145459fb8588ead76725079e28c27bee748
Alberto <alberto101481@gmail.com>
2023-04-13 22:48:01 -0600

    ADD: Navbar and Hero :sparkles:

---
+++ daisyUI-Coaster/src/App.jsx
@@ -0,0 +1,14 @@
+import HeroBatman from './components/HeroBatman';
+import NavBar from './components/NavBar';
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <HeroBatman /> 
+    </>
+  )
+}
+
+export default App
